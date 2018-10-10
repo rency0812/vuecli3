ddfccedcaa19387d0244deb17f171779e0e5210e
rency <you@email.com>
2018-10-10 16:09:02 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{aTime}}</h2>
+    <p>这是使用vuecli3.0构建</p>
+    <p>作者：rency</p>
+    <p>...........</p>
   </div>
 </template>
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      aTime: null
+    }
+  },
+  mounted () {
+    this.aTime = this.$store.state.token
+  },
+  watch: {
+    '$store.state.token' (v) {
+      this.aTime = v
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,38 @@
+<style lang="scss" scoped>
+  .home{
+    h2{
+      font-size: 50px;
+      color: dodgerblue;
+    }
+  }
+</style>
+
 <template>
   <div class="home">
+    <h2>{{nTime}}</h2>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App 3.0"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import moment from 'moment'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      nTime: null
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.nTime = moment().format('YYYY MM DD HH:mm:ss')
+      this.$store.state.token = this.nTime
+    }, 1000)
+  },
   components: {
     HelloWorld
   }
