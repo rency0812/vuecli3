3e7df92344ade8e35e1d1606678b4f026f8ec35a
洛洛 <601062253@qq.com>
2019-12-20 10:59:23 +0800

    Create temp2.vue

---
+++ src/components/temp2.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="temp2">
+  <slot name="temp2">这是一个悲伤的故事</slot>
+</div>
+</template>
+<script>
+export default {
+  name: 'Temp2',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
