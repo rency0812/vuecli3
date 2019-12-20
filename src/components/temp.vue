a8f59e010299e3b765400668d8e571992a5148bb
洛洛 <601062253@qq.com>
2019-12-20 10:58:56 +0800

    Create temp.vue

---
+++ src/components/temp.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="temp">
+  <div>
+    <slot name="head">
+      <p :class="[type]">{{msg}}你好啊</p>
+    </slot>
+  </div>
+  <p>
+    <slot name="main">我好啊</slot>
+  </p>
+</div>
+</template>
+<script>
+export default {
+  name: 'Temp',
+  data () {
+    return {
+      msg: 'Hello',
+      type: null
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.error {
+  color: red;
+}
+</style>
